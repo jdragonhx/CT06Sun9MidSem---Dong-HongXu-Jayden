cf08d85bfc5ad2417508cc1ccb0b60bae9b2b8b3
jdragonhx <jayden.dhx@gmail.com>
2025-08-31 04:34:41 +0000

    31/8

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,13 @@
+function setup(){
+    createCanvas(600, 400);
+    background(0);
+}
 
+function draw() {
+    fill(0, 0, 0);
+    stroke(255, 0, 0);
+    strokeWeight(10);
+    triangle(300, 150, 250, 250, 350, 250);
+    circle(150, 200, 100)
+    rect(400, 150, 100, 100);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,12 @@
+function setup(){
+    createCanvas(600, 600);
+    background(220);
+}
+
+function draw() {
+    for (let i = 0; i < 10; i++) {
+        fill(i * 25, i * 25, i * 25)
+        circle(50 + i * 55, i * 50 , 50);
+}
+    }
 
