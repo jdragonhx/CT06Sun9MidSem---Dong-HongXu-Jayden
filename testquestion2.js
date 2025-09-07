9a1590c2a8c06a2934e0ce807039369cca578727
jdragonhx <jayden.dhx@gmail.com>
2025-09-07 03:25:31 +0000

    Sep 7, 2025, 3:25 AM

--- testquestion2.js
+++ testquestion2.js
@@ -6,7 +6,7 @@ function setup(){
 function draw() {
     for (let i = 0; i < 10; i++) {
         fill(i * 25, i * 25, i * 25)
-        circle(50 + i * 50,50 + i * 50 , 50);
+        circle(50 + i * 50,i * 50 , 50);
 }
     }
 
